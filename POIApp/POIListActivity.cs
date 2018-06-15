f39cfbb5202692ee42b39700ee005e46a3beb2d5
Battye <jba61@ostml0195646.bskyb.com>
2018-06-15 08:15:41 +0100

    initial

---
+++ POIApp/POIListActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace POIApp
+{
+    [Activity(Label = "POIApp", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class POIListActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.POIList);
+        }
+    }
+}
+
---
+++ POIApp/POIListViewAdapter.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using Android.App;
+using Android.Views;
+using Android.Widget;
+
+namespace POIApp
+{
+    public class POIListViewAdapter : BaseAdapter<PointOfInterest>
+    {
+        private readonly Activity context;
+        private List<PointOfInterest> poiListData;
+
+        public POIListViewAdapter(Activity _context, List<PointOfInterest> _poiListData)
+             : base()
+        {
+            this.context = _context;
+            this.poiListData = _poiListData;
+        }
+
+        public override PointOfInterest this[int index]
+        {
+            get
+            {
+                return poiListData[index];
+            }
+        }
+
+        public override int Count
+        {
+            get
+            {
+                return poiListData.Count;
+            }
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            var view = convertView;
+
+            if (view == null)
+            {
+                view = context.LayoutInflater.Inflate(Resource.Layout.POIListItem, null);
+            }
+
+            PointOfInterest poi = this[position];
+            view.FindViewById<TextView>(Resource.Id.nameTextView).Text = poi.Name;
+
+            if (string.IsNullOrEmpty(poi.Address))
+            {
+                view.FindViewById<TextView>(Resource.Id.addrTextView).Visibility = ViewStates.Gone;
+            }
+            else
+            {
+                view.FindViewById<TextView>(Resource.Id.addrTextView).Text = poi.Address;
+            }
+
+            /*var imageView = view.FindViewById<ImageView>(Resource.Id.poiImageView);
+            if (!string.IsNullOrEmpty(poi.Address))
+            {
+                Koush.UrlImageViewHelper.SetUrlDrawable(imageView, poi.Image, Resource.Drawable.ic_placeholder);
+            }*/
+                 
+
+            return view;
+
+        }
+    }
+}
